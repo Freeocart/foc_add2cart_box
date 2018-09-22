8dae9a8cba53507a5cb183162b07fb6f2b4f211d
ikenfin <ikenfin@gmail.com>
2018-09-22 04:10:04 +0400

    added frontend model

--- upload/catalog/model/extension/module/foc_add2cart_box.php
+++ upload/catalog/model/extension/module/foc_add2cart_box.php
@@ -3,19 +3,63 @@ class ModelExtensionModuleFocAdd2cartBox extends Model {
   const SETTINGS_GROUP     = 'foc_add2cart_box';
   const SETTINGS_GROUP_KEY = 'foc_add2cart_box_data';
 
+  private $__settings_loaded = false;
+  private $__settings = null;
+
   public function __construct ($registry) {
     parent::__construct($registry);
     $this->load->model('setting/setting');
   }
 
   public function getSettings () {
+    $this->load->model('setting/setting');
+    $language_id = $this->config->get('config_language_id');
+
     $settings = $this->model_setting_setting->getSetting(self::SETTINGS_GROUP);
 
-    if (is_null($settings) || !isset($settings[self::SETTINGS_GROUP_KEY])) {
-      return $this->defaultSettings();
+    if (!is_null($settings)
+        && isset($settings[self::SETTINGS_GROUP_KEY])
+        && isset($settings[self::SETTINGS_GROUP_KEY][$language_id])
+    ) {
+      $this->__settings = $settings[self::SETTINGS_GROUP_KEY][$language_id];
+      $this->__settings_loaded = true;
     }
-    else {
-      return array_replace_recursive($this->defaultSettings(), $settings[self::SETTINGS_GROUP_KEY]);
-    }
+
+    return $this->__settings;
   }
+
+  public function getByKey ($key) {
+    if (!$this->__settings_loaded) {
+      $this->getSettings();
+    }
+
+    $key = self::SETTINGS_GROUP . '_' . $key;
+
+    if (isset($this->__settings[$key])) {
+      return $this->__settings[$key];
+    }
+
+    return null;
+  }
+
+  public function processTemplate ($key, $data) {
+    $result = $this->getByKey($key);
+
+    foreach ($data as $variable => $value) {
+      $result = str_replace('{{ ' . $variable . ' }}', $value, $result);
+    }
+
+    return $result;
+  }
+
+  public function getModalTitle ($data) {
+    $title = $this->getByKey('foc_add2cart_box_title');
+    return $this->processTemplate($title, $data);
+  }
+
+  public function getModalContent ($data) {
+    $content = $this->getByKey('foc_add2cart_box_content');
+    return $this->processTemplate($content, $data);
+  }
+
 }
